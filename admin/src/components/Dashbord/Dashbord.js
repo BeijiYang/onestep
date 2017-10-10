5380d445022489314552f77463d1a9c4a694867a
BeijiYang <langyuyang@outlook.com>
2017-10-10 19:15:33 +0800

    dashbord layout

--- admin/src/components/Dashbord/Dashbord.js
+++ admin/src/components/Dashbord/Dashbord.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const DashbordWrap = styled.div`
+  border: 2px solid red;
+  height: 100vh;
+  display: flex;
+`
+
+const SideBarWrap = styled.div`
+  border: 2px solid green;
+  flex-basis: 150px;
+`
+
+const ContentWrap = styled.div`
+  border: 2px solid green;
+  flex-grow: 1;
+`
 
 class Dashbord extends Component {
   render () {
     return (
-      <div>
-        Dashbord
-      </div>
+      <DashbordWrap>
+        <SideBarWrap>
+          SideBar
+        </SideBarWrap>
+        <ContentWrap>
+          Content
+        </ContentWrap>
+      </DashbordWrap>
     )
   }
 }
